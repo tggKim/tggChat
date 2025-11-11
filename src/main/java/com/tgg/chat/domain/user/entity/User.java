c957089c1fbf69b8884b1f9e8e47ac46cedfe6e0
tgg <qazplmtfgcv123@gmail.com>
2025-11-12 08:39:11 +0900

    User 엔티티 기본 설계

---
+++ src/main/java/com/tgg/chat/domain/user/entity/User.java
@@ -0,0 +1,54 @@
+package com.tgg.chat.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    private User(String email, String password, String username, Boolean deleted) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.deleted = deleted;
+    }
+
+    public static User of(String email, String password, String username) {
+        return new User(email, password, username, false);
+    }
+
+}
