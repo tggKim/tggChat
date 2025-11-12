547df68788fee203b3b88d9bc31798e9321f7b09
admin <qazplmtfgcv123@gamil.com>
2025-11-12 14:20:32 +0900

    feat: ErrorResponse에 of() 오버로딩하여 커스텀 메시지 담을 수 있도록 작성

--- src/main/java/com/tgg/chat/exception/ErrorResponse.java
+++ src/main/java/com/tgg/chat/exception/ErrorResponse.java
@@ -12,20 +12,29 @@ public class ErrorResponse {
 	private final String message;
 	private final LocalDateTime timestamp;
 	
-	private ErrorResponse (String code, int status, String message, LocalDateTime timestamp) {
+	private ErrorResponse(String code, int status, String message, LocalDateTime timestamp) {
 		this.code = code;
 		this.status = status;
 		this.message = message;
 		this.timestamp = timestamp;
 	}
 	
-	public static ErrorResponse of (ErrorCode errorCode) {
+	public static ErrorResponse of(ErrorCode errorCode) {
 		
 		return new ErrorResponse(errorCode.getCode(),
 									errorCode.getStatus().value(),
 									errorCode.getMessage(),
 									LocalDateTime.now());
 		
+		
+	}
+	
+	public static ErrorResponse of(ErrorCode errorCode, String message) {
+		
+		return new ErrorResponse(errorCode.getCode(),
+				errorCode.getStatus().value(),
+				message,
+				LocalDateTime.now());
 		
 	}
 	
