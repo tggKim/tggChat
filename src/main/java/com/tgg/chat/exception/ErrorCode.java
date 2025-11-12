34777e9e1adc780e8314e77212f1bf1fe5e8d859
admin <qazplmtfgcv123@gamil.com>
2025-11-12 14:16:12 +0900

    feat: ErrorCode에 서버 내부 처리용 INTERNAL_SERVER_ERROR 추가 #2

--- src/main/java/com/tgg/chat/exception/ErrorCode.java
+++ src/main/java/com/tgg/chat/exception/ErrorCode.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 	
+	// 서버 에러
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다."),
+	
 	// 유저
 	NOT_VALID_EMAIL_ERROR(HttpStatus.BAD_REQUEST, "U001", "올바르지 않은 이메일 형식 입니다."),
 	DUPLICATE_EMAIL_ERROR(HttpStatus.CONFLICT, "U002", "중복된 이메일 입니다.");
