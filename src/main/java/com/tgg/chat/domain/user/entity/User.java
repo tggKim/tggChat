3f3be2fb26367ee3d8ea1d22b7a565789d2d0661
admin <qazplmtfgcv123@gamil.com>
2025-11-12 10:52:03 +0900

    fix: email 컬럼 길이 제약 수정

--- src/main/java/com/tgg/chat/domain/user/entity/User.java
+++ src/main/java/com/tgg/chat/domain/user/entity/User.java
@@ -19,7 +19,7 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false, length = 100)
+    @Column(unique = true, nullable = false, length = 254)
     private String email;
 
     @Column(nullable = false)
