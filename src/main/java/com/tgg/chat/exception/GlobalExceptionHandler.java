981c3674d734a50564911503d5954f902508db4f
admin <qazplmtfgcv123@gamil.com>
2025-11-12 15:19:06 +0900

    feat: MethodArgumentNotValidException 예외 핸들러 작성 #2

--- src/main/java/com/tgg/chat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tgg/chat/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.tgg.chat.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,7 +19,7 @@ public class GlobalExceptionHandler {
 		
         log.warn("[ErrorException] code={}, status={}, message={}",
                 errorCode.getCode(),
-                errorCode.getStatus(),
+                errorCode.getStatus().value(),
                 e.getMessage());
 		
 		return ResponseEntity
@@ -34,6 +36,29 @@ public class GlobalExceptionHandler {
 		return ResponseEntity
 				.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
 				.body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		
+		String errorMessage = e.getBindingResult()
+			    .getFieldErrors()
+			    .stream()
+			    .findFirst()
+			    .map(FieldError::getDefaultMessage)
+			    .orElse("입력값이 올바르지 않습니다.");
+		
+		ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+		
+        log.warn("[ValidationError] code={}, status={}, message={}",
+                errorCode.getCode(),
+                errorCode.getStatus().value(),
+                errorMessage);
+        
+        return ResponseEntity
+        		.status(errorCode.getStatus())
+        		.body(ErrorResponse.of(errorCode, errorMessage));
 		
 	}
 
