9983c734c0f0e003f21ab1d7f98291d0005a5975
admin <qazplmtfgcv123@gamil.com>
2025-11-12 13:11:45 +0900

    feat: 예외 응답 ErrorResponse 작성 #2

---
+++ src/main/java/com/tgg/chat/exception/ErrorResponse.java
@@ -0,0 +1,32 @@
+package com.tgg.chat.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+	
+	private final String code;
+	private final int status;
+	private final String message;
+	private final LocalDateTime timestamp;
+	
+	private ErrorResponse (String code, int status, String message, LocalDateTime timestamp) {
+		this.code = code;
+		this.status = status;
+		this.message = message;
+		this.timestamp = timestamp;
+	}
+	
+	public static ErrorResponse of (ErrorCode errorCode) {
+		
+		return new ErrorResponse(errorCode.getCode(),
+									errorCode.getStatus().value(),
+									errorCode.getMessage(),
+									LocalDateTime.now());
+		
+		
+	}
+	
+}
