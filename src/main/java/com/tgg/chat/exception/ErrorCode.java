78e6246e72b3ec047db7d9ea6cd2878941f84c34
admin <qazplmtfgcv123@gamil.com>
2025-11-12 14:30:51 +0900

    feat: ErrorCode에 사용자 입력값 오류용 INVALID_INPUT_VALUE 추가 #2

--- src/main/java/com/tgg/chat/exception/ErrorCode.java
+++ src/main/java/com/tgg/chat/exception/ErrorCode.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 	
-	// 서버 에러
+	// 공통(Common)
+	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "요청 값이 유효하지 않습니다."),
+	
+	// 서버 에러(Server)
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다."),
 	
-	// 유저
-	NOT_VALID_EMAIL_ERROR(HttpStatus.BAD_REQUEST, "U001", "올바르지 않은 이메일 형식 입니다."),
-	DUPLICATE_EMAIL_ERROR(HttpStatus.CONFLICT, "U002", "중복된 이메일 입니다.");
+	// 유저(User)
+	DUPLICATE_EMAIL_ERROR(HttpStatus.CONFLICT, "U001", "중복된 이메일 입니다.");
 	
 	private ErrorCode(HttpStatus status, String code, String message) {
 		this.status = status;
