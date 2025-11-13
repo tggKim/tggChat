9cbcef91583b4163f65953462f6e61ddf91224b1
admin <qazplmtfgcv123@gamil.com>
2025-11-13 14:38:04 +0900

    feat: 이메일 중복 체크 로직 작성 #4

--- src/main/java/com/tgg/chat/domain/user/service/UserService.java
+++ src/main/java/com/tgg/chat/domain/user/service/UserService.java
@@ -7,6 +7,8 @@ import com.tgg.chat.domain.user.dto.response.SignUpResponseDto;
 import com.tgg.chat.domain.user.entity.User;
 import com.tgg.chat.domain.user.repository.UserMapper;
 import com.tgg.chat.domain.user.repository.UserRepository;
+import com.tgg.chat.exception.ErrorCode;
+import com.tgg.chat.exception.ErrorException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +20,11 @@ public class UserService {
 	private final UserMapper userMapper;
 	
 	public SignUpResponseDto signUpUser(SignUpRequestDto signUpRequestDto) {
+		
+		// 이메일 중복 검사
+		if(userMapper.existsByEmail(signUpRequestDto.getEmail())) {
+			throw new ErrorException(ErrorCode.DUPLICATE_EMAIL_ERROR);
+		}
 		
 		User requestUser = User.of(signUpRequestDto.getEmail(), signUpRequestDto.getPassword(), signUpRequestDto.getUsername());
 		
