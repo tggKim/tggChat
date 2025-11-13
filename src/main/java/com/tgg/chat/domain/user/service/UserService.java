28ba9c1245ace5be17cc2adf8e70db2907b06b6b
admin <qazplmtfgcv123@gamil.com>
2025-11-13 13:50:53 +0900

    feat: UserService에 회원가입 로직 추가 #4

--- src/main/java/com/tgg/chat/domain/user/service/UserService.java
+++ src/main/java/com/tgg/chat/domain/user/service/UserService.java
@@ -2,6 +2,10 @@ package com.tgg.chat.domain.user.service;
 
 import org.springframework.stereotype.Service;
 
+import com.tgg.chat.domain.user.dto.request.SignUpRequestDto;
+import com.tgg.chat.domain.user.dto.response.SignUpResponseDto;
+import com.tgg.chat.domain.user.entity.User;
+import com.tgg.chat.domain.user.repository.UserMapper;
 import com.tgg.chat.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +15,16 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final UserMapper userMapper;
+	
+	public SignUpResponseDto saveUser(SignUpRequestDto signUpRequestDto) {
+		
+		User requestUser = User.of(signUpRequestDto.getEmail(), signUpRequestDto.getPassword(), signUpRequestDto.getUsername());
+		
+		User savedUser = userRepository.save(requestUser);
+		
+		return SignUpResponseDto.of(savedUser);
+		
+	}
 	
 }
