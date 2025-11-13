5b3a6d737d0678070b79b7f25f31a577d344e47e
tgg <qazplmtfgcv123@gmail.com>
2025-11-13 23:56:22 +0900

    feat: 유저 수정 로직 작성 #4

---
+++ src/main/java/com/tgg/chat/domain/user/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package com.tgg.chat.domain.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserUpdateRequestDto {
+
+    private Long id;
+
+    @NotBlank(message = "사용자명은 필수입니다.")
+    @Size(max = 50, message = "사용자명 길이는 50자 이하입니다.")
+    private String username;
+
+}
