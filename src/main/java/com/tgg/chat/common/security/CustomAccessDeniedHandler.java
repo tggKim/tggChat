5159ff81017009ec8de57311c3400869d4e79b5e
admin <qazplmtfgcv123@gamil.com>
2025-11-17 18:03:18 +0900

    feat: 권한 없을 시 예외 처리 위해 AccessDeniedHandler 인터페이스 구현 #5

---
+++ src/main/java/com/tgg/chat/common/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.tgg.chat.common.security;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler{
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
