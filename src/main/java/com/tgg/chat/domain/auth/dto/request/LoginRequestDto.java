1934a6d57bac84019c4e9c877d057dacbd5a933c
admin <qazplmtfgcv123@gamil.com>
2025-11-18 10:58:44 +0900

    feat: 로그인 응답 DTO 작성 #6

--- src/main/java/com/tgg/chat/domain/auth/dto/request/LoginRequestDto.java
+++ src/main/java/com/tgg/chat/domain/auth/dto/request/LoginRequestDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @Schema(description = "로그인 요청 DTO")
 public class LoginRequestDto {
 
---
+++ src/main/java/com/tgg/chat/domain/auth/dto/response/LoginResponseDto.java
@@ -0,0 +1,25 @@
+package com.tgg.chat.domain.auth.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "로그인 응답 DTO")
+public class LoginResponseDto {
+	
+	@Schema(description = "엑세스 토큰")
+	private final String accessToken;
+	
+	@Schema(description = "리프레시 토큰")
+	private final String refreshToken;
+
+	private LoginResponseDto(String accessToken, String refreshToken) {	
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+	
+	public LoginResponseDto of(String accessToken, String resfreshToken) {
+		return new LoginResponseDto(accessToken, resfreshToken);
+	}
+	
+}
