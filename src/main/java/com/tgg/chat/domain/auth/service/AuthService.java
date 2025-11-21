012e166f81056782ddf98bf28ead221016e3a8f9
admin <qazplmtfgcv123@gamil.com>
2025-11-21 11:28:12 +0900

    feat: 로그인시 토큰 레디스에 저장되도록 로직 수정 #6

--- src/main/java/com/tgg/chat/domain/auth/service/AuthService.java
+++ src/main/java/com/tgg/chat/domain/auth/service/AuthService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.tgg.chat.common.jwt.JwtUtils;
+import com.tgg.chat.common.redis.RedisUtils;
 import com.tgg.chat.domain.auth.dto.request.LoginRequestDto;
 import com.tgg.chat.domain.auth.dto.response.LoginResponseDto;
 import com.tgg.chat.domain.user.entity.User;
@@ -12,6 +13,7 @@ import com.tgg.chat.domain.user.repository.UserRepository;
 import com.tgg.chat.exception.ErrorCode;
 import com.tgg.chat.exception.ErrorException;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,6 +24,7 @@ public class AuthService {
 	private final UserRepository userRepository;
 	private final UserMapper userMapper;
 	private final PasswordEncoder passwordEncoder;
+	private final RedisUtils redisUtils;
 	
 	public LoginResponseDto login(LoginRequestDto loginRequestDto) {
 		
@@ -39,7 +42,28 @@ public class AuthService {
 		String accessToken = jwtUtils.createAccessToken(findUser);
 		String refreshToken = jwtUtils.createRefreshToken(findUser);
 		
+		// AccessToken, RefreshToken 레디스에 저장
+		storeTokenSet(findUser.getUserId(), accessToken, refreshToken);
+		
 		return LoginResponseDto.of(accessToken, refreshToken);
+		
+	}
+	
+	private void storeTokenSet(Long userId, String accessToken, String refreshToken) {
+
+		Claims accessTokenClaims = jwtUtils.getClaims(accessToken);
+		Claims refreshTokenClaims = jwtUtils.getClaims(refreshToken);
+		
+		Long accessTokenExpireTime = accessTokenClaims.getExpiration().getTime();
+		Long refreshTokenExpireTime = refreshTokenClaims.getExpiration().getTime();
+		
+		Long currentTime = System.currentTimeMillis();
+		
+		Long accessTokenTTL = accessTokenExpireTime - currentTime;
+		Long refreshTokenTTL = refreshTokenExpireTime - currentTime;
+		
+		redisUtils.saveAccessToken(userId , accessToken, accessTokenTTL);
+		redisUtils.saveRefreshToken(userId, refreshToken, refreshTokenTTL);
 		
 	}
 	
