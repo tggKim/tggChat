9d2e9a260c3473cc0b061b92a9a1d0e09ba60780
admin <qazplmtfgcv123@gamil.com>
2025-11-21 14:34:02 +0900

    feat: 로그인 여부 요청 DTO 작성 #6

---
+++ src/main/java/com/tgg/chat/domain/auth/dto/request/LoginStatusRequestDto.java
@@ -0,0 +1,21 @@
+package com.tgg.chat.domain.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "로그인 여부 요청 DTO")
+public class LoginStatusRequestDto {
+
+	@Email(message = "올바른 이메일 형식이 아닙니다.")
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Size(max = 254, message = "이메일 길이는 254자 이하입니다.")
+	@Schema(description = "이메일", example = "test@example.com")
+	private String email;
+	
+}
