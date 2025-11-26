099a979082cf64e7778d159046606fa8944cf96d
admin <qazplmtfgcv123@gamil.com>
2025-11-26 17:33:54 +0900

    feat: JwtChannelInterceptor 에서 JWT 검증 로직 작성 #7

--- src/main/java/com/tgg/chat/common/stomp/JwtChannelInterceptor.java
+++ src/main/java/com/tgg/chat/common/stomp/JwtChannelInterceptor.java
@@ -8,9 +8,21 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
+import com.tgg.chat.common.jwt.JwtUtils;
+import com.tgg.chat.common.redis.RedisUtils;
+import com.tgg.chat.exception.ErrorCode;
+import com.tgg.chat.exception.ErrorException;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class JwtChannelInterceptor implements ChannelInterceptor {
 
+	private final JwtUtils jwtUtils;
+	private final RedisUtils redisUtils;
+	
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
@@ -18,12 +30,32 @@ public class JwtChannelInterceptor implements ChannelInterceptor {
         if(StompCommand.CONNECT.equals(accessor.getCommand())) {
 
             Object tokenObject = accessor.getFirstNativeHeader("Authorization");
+            
             if(tokenObject == null) {
+        		throw new ErrorException(ErrorCode.JWT_MISSING_AUTH_HEADER);
+        	}
+        	
+        	String bearerString = tokenObject.toString();
+        	
+        	if(!bearerString.startsWith("Bearer ")) {
+				throw new ErrorException(ErrorCode.JWT_INVALID_AUTH_SCHEME);
+			}
+        	
+			String jwtString = bearerString.substring(7);
 
-            }
+			jwtUtils.validateToken(jwtString);
+			
+			// claims 추출
+			Claims claims = jwtUtils.getClaims(jwtString);
+			
+			// 레디스에 저장된 accessToken 과 비교
+			Long userId = Long.parseLong(claims.getSubject());
+			String redisAccessToken = redisUtils.getAccessToken(userId);
+			if(redisAccessToken == null || !redisAccessToken.equals(jwtString)) {
+				throw new ErrorException(ErrorCode.ACCESS_TOKEN_MISMATCH);
+			}
 
-            String token = tokenObject.toString();
-            System.out.println(token);
+            System.out.println(claims);
 
         }
 
