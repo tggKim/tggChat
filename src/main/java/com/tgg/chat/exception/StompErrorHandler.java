28c2304e75e2bb997bc5ed93d08236cb815240ff
admin <qazplmtfgcv123@gamil.com>
2025-12-19 16:58:12 +0900

    refactor: StompErrorHandler 구조 리팩터링 #7

--- src/main/java/com/tgg/chat/exception/StompErrorHandler.java
+++ src/main/java/com/tgg/chat/exception/StompErrorHandler.java
@@ -1,5 +1,6 @@
 package com.tgg.chat.exception;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -27,58 +28,70 @@ public class StompErrorHandler extends StompSubProtocolErrorHandler{
 	@Override
 	public Message<byte[]> handleClientMessageProcessingError(Message<byte[]> clientMessage, Throwable ex) {
 		
-		if(ex.getCause() instanceof ErrorException errorException) {
+		Throwable stompError = ex.getCause();
+		
+		ErrorCode errorCode = null;
+		
+		// ErrorException 이면 그에 맞는 예외 처리, ErrorException 이 아니라면 서버에러(500) 리턴
+		if(stompError instanceof ErrorException errorException) {
 			
-			ErrorCode errorCode = errorException.getErrorCode();
-			String errorMessage = errorCode.getMessage();
+			errorCode = errorException.getErrorCode();
 			
 	        log.warn("[ErrorException] code={}, status={}, message={}",
 	                errorCode.getCode(),
 	                errorCode.getStatus().value(),
-	                errorMessage);
-	        
-	        ErrorResponse errorResponse = ErrorResponse.of(errorCode);
-	        
-	        byte[] errorResponseByte = null;
-	        try {
-	        	
-				errorResponseByte = objectMapper.writeValueAsBytes(errorResponse);
-				
-			} catch (JsonProcessingException e) {
-				
-				ErrorCode internalServerError = ErrorCode.INTERNAL_SERVER_ERROR;
-				String internalServerErrorMessage = internalServerError.getMessage();
-				
-		        log.error("[Unhandled Exception] code={}, status={}, message={}",
-		        		internalServerError.getCode(),
-		        		internalServerError.getStatus().value(),
-		        		internalServerErrorMessage);
-			    
-		        String timestamp = LocalDateTime.now()
-		                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-		        
-				String fallbackJson = String.format(
-			            "{\"code\":\"%s\",\"status\":%d,\"message\":\"%s\",\"timestamp\":\"%s\"}",
-			            internalServerError.getCode(),
-			            internalServerError.getStatus().value(),
-			            internalServerError.getMessage(),
-			            timestamp
-			    );
-				
-				errorResponseByte = fallbackJson.getBytes();
-				
-			}
-		
-			StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
+	                errorCode.getMessage());
 			
-			Message<byte[]> message = MessageBuilder.createMessage(errorResponseByte, accessor.getMessageHeaders());
-
-			return message;
+		} else {
+			
+			errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+			
+	        log.error("[UnhandledException] code={}, status={}, message={}",
+	                errorCode.getCode(),
+	                errorCode.getStatus().value(),
+	                errorCode.getMessage());
 			
 		}
 		
-		return super.handleClientMessageProcessingError(clientMessage, ex);
+		ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+		byte[] errorResponseByte = errorResponseToByte(errorResponse);
+		
+		StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.ERROR);
+		Message<byte[]> message = MessageBuilder.createMessage(errorResponseByte, accessor.getMessageHeaders());
+
+		return message;
 	
+	}
+	
+	private byte[] errorResponseToByte(ErrorResponse errorResponse) {
+		
+		try {
+			
+			return objectMapper.writeValueAsBytes(errorResponse);
+			
+		} catch (JsonProcessingException e) {
+			
+			ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+			
+	        log.error("[Unhandled Exception] code={}, status={}, message={}",
+	        		errorCode.getCode(),
+	        		errorCode.getStatus().value(),
+	        		errorCode.getMessage());
+		    
+	        String timestamp = LocalDateTime.now()
+	                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	        
+			String fallbackJson = String.format(
+		            "{\"code\":\"%s\",\"status\":%d,\"message\":\"%s\",\"timestamp\":\"%s\"}",
+		            errorCode.getCode(),
+		            errorCode.getStatus().value(),
+		            errorCode.getMessage(),
+		            timestamp);
+			
+			return fallbackJson.getBytes(StandardCharsets.UTF_8);
+			
+		}
+		
 	}
 	
 }
