da6c835442be2a7bbac09ab03b25b97749216d2f
tgg <qazplmtfgcv123@gmail.com>
2026-01-07 22:13:09 +0900

    fix: UserFriendMapper 클래스에서 친구목록 조회 메서드 리턴 타입 변경 #8

--- src/main/java/com/tgg/chat/domain/friend/repository/UserFriendMapper.java
+++ src/main/java/com/tgg/chat/domain/friend/repository/UserFriendMapper.java
@@ -1,6 +1,6 @@
 package com.tgg.chat.domain.friend.repository;
 
-import com.tgg.chat.domain.friend.entity.UserFriend;
+import com.tgg.chat.domain.friend.dto.query.UserFriendRowDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,6 +8,6 @@ import java.util.List;
 @Mapper
 public interface UserFriendMapper {
 
-    public List<UserFriend> findOwnerId(Long ownerId);
+    public List<UserFriendRowDto> findFriendListByOwnerId(Long ownerId);
 
 }
