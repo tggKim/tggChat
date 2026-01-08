31eb3f9c09d2b8bb68b8066710bf50d2553e3833
admin <qazplmtfgcv123@gamil.com>
2026-01-08 14:12:12 +0900

    feat: user 엔티티의 username 필드에 유니크 제약조건 추가 #8

--- src/main/java/com/tgg/chat/domain/user/entity/User.java
+++ src/main/java/com/tgg/chat/domain/user/entity/User.java
@@ -25,7 +25,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, length = 50)
+    @Column(unique = true, nullable = false, length = 50)
     private String username;
 
     @Column(nullable = false)
