d440d5e7532acebc5fa0ccb0d5f983cc6e7bea0d
admin <qazplmtfgcv123@gamil.com>
2026-01-08 11:17:29 +0900

    feat: 친구 목록 추가 스웨거 작성

--- src/main/java/com/tgg/chat/domain/friend/controller/UserFriendController.java
+++ src/main/java/com/tgg/chat/domain/friend/controller/UserFriendController.java
@@ -2,7 +2,16 @@ package com.tgg.chat.domain.friend.controller;
 
 import com.tgg.chat.domain.friend.dto.request.CreateFriendRequestDto;
 import com.tgg.chat.domain.friend.service.UserFriendService;
+import com.tgg.chat.domain.user.dto.response.SignUpResponseDto;
+import com.tgg.chat.exception.ErrorResponse;
+
 import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Friend API", description = "친구 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class UserFriendController {
@@ -18,12 +28,42 @@ public class UserFriendController {
     private final UserFriendService userFriendService;
 
     @PostMapping("/friends")
+	@Operation(
+		summary = "친구 추가",
+		description =  "유저를 친구 목록에 추가합니다."
+	)
+	@ApiResponses({
+		@ApiResponse(
+				responseCode = "200", 
+				description = "친구 추가 성공",
+				content = @Content(
+					mediaType = "application/json"
+				)
+		),
+		@ApiResponse(
+				responseCode = "404", 
+				description = "존재하지 않는 유저",
+				content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ErrorResponse.class)
+				)
+		),
+		@ApiResponse(
+				responseCode = "409", 
+				description = "이미 친구로 등록된 유저",
+				content = @Content(
+					mediaType = "application/json",
+					schema = @Schema(implementation = ErrorResponse.class)
+				)
+		)
+	})
     public ResponseEntity<Void> createUserFriend(Authentication authentication, @RequestBody CreateFriendRequestDto createFriendRequestDto) {
 
+    	// Authentication 에서 로그인한 유저의 userId 추출
         Claims claims = (Claims)authentication.getPrincipal();
-
         Long loginUserId = Long.parseLong(claims.getSubject());
 
+        // 친구 추가
         userFriendService.createFriend(loginUserId, createFriendRequestDto);
 
         return ResponseEntity
