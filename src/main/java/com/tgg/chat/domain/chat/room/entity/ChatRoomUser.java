f46f6620ea788184537c89183fb5c922d5807d25
admin <qazplmtfgcv123@gamil.com>
2026-01-12 17:48:32 +0900

    feat: 유저와 채팅방 매핑하기 위한 ChatRoomUser 엔티티 작성

---
+++ src/main/java/com/tgg/chat/domain/chat/room/entity/ChatRoomUser.java
@@ -0,0 +1,23 @@
+package com.tgg.chat.domain.chat.room.entity;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoomUser {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long chatRoomUserId;
+	
+}
