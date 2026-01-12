622172ec8d4595de96e79c33892bae09eabcb93d
admin <qazplmtfgcv123@gamil.com>
2026-01-12 17:59:13 +0900

    feat: ChatRoomUser에 fk 추가하고 생성일, 수정일 필드 추가 #9

--- src/main/java/com/tgg/chat/domain/chat/room/entity/ChatRoomUser.java
+++ src/main/java/com/tgg/chat/domain/chat/room/entity/ChatRoomUser.java
@@ -1,12 +1,22 @@
 package com.tgg.chat.domain.chat.room.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.tgg.chat.domain.user.entity.User;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,5 +29,23 @@ public class ChatRoomUser {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long chatRoomUserId;
+	
+	// 유저를 구분하기 위한 fk 값
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	// 채팅방을 구분하기 위한 fk 값
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chat_room_id")
+	private ChatRoom chatRoom;
+	
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
 	
 }
