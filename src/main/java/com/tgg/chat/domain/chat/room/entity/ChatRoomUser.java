2ff9237789e793c80486095bc15c3caf5eca9dec
admin <qazplmtfgcv123@gamil.com>
2026-01-13 17:58:06 +0900

    feat: 채팅방 참여 사람 권한, 참여 상태 Enum 작성 #9

--- src/main/java/com/tgg/chat/domain/chat/room/entity/ChatRoomUser.java
+++ src/main/java/com/tgg/chat/domain/chat/room/entity/ChatRoomUser.java
@@ -6,11 +6,15 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.tgg.chat.domain.chat.room.enums.ChatRoomUserRole;
+import com.tgg.chat.domain.chat.room.enums.ChatRoomUserStatus;
 import com.tgg.chat.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -49,6 +53,17 @@ public class ChatRoomUser {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "chat_room_id")
 	private ChatRoom chatRoom;
+	
+	// 채팅에 참여한 사람 권한 -> OWNER, MEMBER 2가지이며 1대1 채팅에서는 둘 다 MEMBER
+	@Enumerated(EnumType.STRING)
+	private ChatRoomUserRole chatRoomUserRole;
+	
+	// 채팅에 참여한 사람 상태 -> ACTIVE, LEFT 2가지
+	@Enumerated(EnumType.STRING)
+	private ChatRoomUserStatus chatRoomUserStatus;
+	
+	// 채팅에 참여한 시점 -> 메시지 불러오는 기준
+	private LocalDateTime joinedAt;
 	
 	@CreatedDate
 	@Column(nullable = false, updatable = false)
---
+++ src/main/java/com/tgg/chat/domain/chat/room/enums/ChatRoomUserStatus.java
@@ -0,0 +1,5 @@
+package com.tgg.chat.domain.chat.room.enums;
+
+public enum ChatRoomUserStatus {
+	ACTIVE, LEFT
+}
