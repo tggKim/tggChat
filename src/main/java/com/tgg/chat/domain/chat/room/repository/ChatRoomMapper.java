8c120b1a8e074190bbece0638ea580ccaeaed21c
tgg <qazplmtfgcv123@gmail.com>
2026-01-18 22:28:58 +0900

    fix: ChatRoomMapper 이 xml 파일과 매핑이 되지 않는 문제 수정 #12

--- src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomMapper.java
+++ src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomMapper.java
@@ -1,8 +1,9 @@
 package com.tgg.chat.domain.chat.room.repository;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Mapper
 public interface ChatRoomMapper {
 
     public boolean existsDirectChatRoom(Long userId1, Long userId2);
