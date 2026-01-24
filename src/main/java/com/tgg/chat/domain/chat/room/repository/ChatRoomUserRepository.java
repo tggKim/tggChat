f86fa56fe37556da8fac947640a798d9d158d680
tgg <qazplmtfgcv123@gmail.com>
2026-01-25 02:17:31 +0900

    feat: JpaRepository로 친구 목록 조회 메서드 생성 #12

--- src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomUserRepository.java
+++ src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomUserRepository.java
@@ -2,8 +2,21 @@ package com.tgg.chat.domain.chat.room.repository;
 
 import com.tgg.chat.domain.chat.room.entity.ChatRoomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, Long> {
+
+    @Query("""
+        select cru
+        from ChatRoomUser cru
+        join fetch cru.user u
+        where cru.chatRoom.chatRoomId = :chatRoomId
+        and cru.user.userId in :friendIds
+    """)
+    List<ChatRoomUser> findByChatRoomIdAndFriendIds(Long chatRoomId, List<Long> friendIds);
+
 }
