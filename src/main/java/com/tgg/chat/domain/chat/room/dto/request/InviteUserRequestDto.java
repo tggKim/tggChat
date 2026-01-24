25ca1eb6192a610801aa9479146e0eed6010c414
tgg <qazplmtfgcv123@gmail.com>
2026-01-24 22:30:17 +0900

    feat: 채팅방 초대 요청 DTO 작성 #12

---
+++ src/main/java/com/tgg/chat/domain/chat/room/dto/request/InviteUserRequestDto.java
@@ -0,0 +1,24 @@
+package com.tgg.chat.domain.chat.room.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "채팅방 초대 요청 DTO")
+public class InviteUserRequestDto {
+
+    @Schema(description = "채팅방에 초대할 참여할 userId 목록", example = "{1,2,3}")
+    @NotNull(message = "friendIds는 필수입니다.")
+    private List<Long> friendIds;
+
+    @Schema(description = "채팅방 id", example = "1")
+    @NotBlank(message = "chatRoomId는 필수입니다.")
+    private Long chatRoomId;
+
+}
