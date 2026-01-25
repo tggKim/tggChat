8da2b0ed2a895ed778a10d561fd27679577e2c91
tgg <qazplmtfgcv123@gmail.com>
2026-01-25 21:56:47 +0900

    feat: ChatRoomRepository에 ChatRoom 조회 메서드 작성 #12

--- src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomRepository.java
+++ src/main/java/com/tgg/chat/domain/chat/room/repository/ChatRoomRepository.java
@@ -1,10 +1,16 @@
 package com.tgg.chat.domain.chat.room.repository;
 
 import com.tgg.chat.domain.chat.room.entity.ChatRoom;
+import com.tgg.chat.domain.chat.room.enums.ChatRoomType;
+import com.tgg.chat.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    Optional<ChatRoom> findByChatRoomTypeAndDirectUser1AndDirectUser2(ChatRoomType chatRoomType, User directUser1, User directUser2);
 
 }
