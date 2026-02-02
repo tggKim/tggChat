e5d0e44780057ea94efbbb57a7265a00f046bbae
DESKTOP-4V92CBJ\admin <qazplmtfgcv123@gamil.com>
2026-02-02 14:28:50 +0900

    feat: 채팅방 나가기 요청 DTO 작성 #12

---
+++ src/main/java/com/tgg/chat/domain/chat/room/dto/request/LeaveChatRoomRequestDto.java
@@ -0,0 +1,23 @@
+package com.tgg.chat.domain.chat.room.dto.request;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "채팅방 나가기 요청 DTO")
+public class LeaveChatRoomRequestDto {
+
+    @Schema(description = "채팅방 id", example = "1")
+    @NotNull(message = "chatRoomId는 필수입니다.")
+    private Long chatRoomId;
+    
+    @Schema(description = "방장 권한 양도할 유저 id, 일반유저가 나가기 요청시 0으로 보낸다.", example = "1")
+    @NotNull(message = "nextOwnerId는 필수입니다.")
+    private Long nextOwnerId;
+
+}
