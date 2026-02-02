39e46470f6fea85603a9751a802d6b00665d9518
DESKTOP-4V92CBJ\admin <qazplmtfgcv123@gamil.com>
2026-02-02 13:33:14 +0900

    feat: 채팅방 나가기 관련 ErrorCode 작성 #12

--- src/main/java/com/tgg/chat/exception/ErrorCode.java
+++ src/main/java/com/tgg/chat/exception/ErrorCode.java
@@ -43,7 +43,9 @@ public enum ErrorCode {
     CHAT_ROOM_INVITE_MEMBER_REQUIRED(HttpStatus.BAD_REQUEST, "CR006", "채팅방 초대는 1명 이상이 필요합니다."),
     CANNOT_INVITE_CHAT_ROOM_WITH_SELF(HttpStatus.BAD_REQUEST, "CR007", "자기 자신을 채팅방에 초대할 수 없습니다."),
     CHAT_ROOM_INVITE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "CR008", "단체 채팅방의 멤버이면서 방장이어야 친구를 초대할 수 있습니다."),
-    CHAT_ROOM_INVITE_ALREADY_MEMBER(HttpStatus.CONFLICT, "CR009", "이미 채팅방에 참여 중인 사용자가 포함되어 있습니다.");
+    CHAT_ROOM_INVITE_ALREADY_MEMBER(HttpStatus.CONFLICT, "CR009", "이미 채팅방에 참여 중인 사용자가 포함되어 있습니다."),
+    CHAT_ROOM_ACCESS_DENIED(HttpStatus.BAD_REQUEST, "CR010", "채팅방이 존재하지 않거나, 채팅방의 유저가 아닙니다."),
+    CHAT_ROOM_NEXT_OWNER_INVALID(HttpStatus.BAD_REQUEST, "CR011", "방장을 양도할 수 없는 멤버입니다.");
 
 	private ErrorCode(HttpStatus status, String code, String message) {
 		this.status = status;
