733d669177b31dcfcbcf3700290e4400ba8b6d46
tgg <qazplmtfgcv123@gmail.com>
2026-02-07 17:07:34 +0900

    feat: ChatController 매칭 경로 수정, redisPublisher 로 메시지 발행하도록 수정 #7

--- src/main/java/com/tgg/chat/domain/chat/controller/ChatController.java
+++ src/main/java/com/tgg/chat/domain/chat/controller/ChatController.java
@@ -2,6 +2,9 @@ package com.tgg.chat.domain.chat.controller;
 
 import java.security.Principal;
 
+import com.tgg.chat.common.redis.pubsub.ChatEvent;
+import com.tgg.chat.common.redis.pubsub.RedisPublisher;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -14,14 +17,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatController {
 
-	private final SimpMessagingTemplate template;
+    private final RedisPublisher redisPublisher;
 	
-	@MessageMapping("/chat.send")
-	public void sendMessage(ChatMessageRequest message, Principal principal) {
-		
-		System.out.println(principal.getName());
-		
-		template.convertAndSend("/topic/" + message.getRoomId(), message);
+	@MessageMapping("/chatRooms/{chatRoomId}/message")
+	public void sendMessage(
+            @DestinationVariable Long chatRoomId,
+            ChatMessageRequest message,
+            Principal principal
+    ) {
+
+        ChatEvent chatEvent = ChatEvent.of(chatRoomId, message.getSenderId(), message.getContent());
+
+		redisPublisher.publishChatEvent(chatEvent);
+
 	}
 	
 }
