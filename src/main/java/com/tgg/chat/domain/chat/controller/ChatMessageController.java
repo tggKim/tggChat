e60121382a8eb22e73a5e067322ceb073c0a32f8
DESKTOP-4V92CBJ\admin <qazplmtfgcv123@gamil.com>
2026-03-06 10:58:24 +0900

    chore: 채팅방 메시지 목록 리턴 컨트롤러 스웨거 수정

--- src/main/java/com/tgg/chat/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/tgg/chat/domain/chat/controller/ChatMessageController.java
@@ -12,10 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tgg.chat.domain.chat.dto.request.ChatMessageListRequestDto;
 import com.tgg.chat.domain.chat.dto.response.ChatMessageListResponseDto;
+import com.tgg.chat.domain.chat.dto.response.CreateDirectChatRoomResponseDto;
 import com.tgg.chat.domain.chat.entity.ChatMessage;
 import com.tgg.chat.domain.chat.service.ChatMessageService;
+import com.tgg.chat.exception.ErrorResponse;
 
 import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +35,37 @@ public class ChatMessageController {
 	private final ChatMessageService chatMessageService;
 	
 	@GetMapping("/chatMessages")
+    @SecurityRequirement(name = "JWT Auth")
+    @Operation(
+            summary = "채팅방 메시지 리스트 반환",
+            description =  "채팅방 메시지 리스트를 반환 합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "채팅방 메시지 리스트를 반환 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ChatMessageListResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "chatRoomId 은 필수입니다.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "offsetSeq 은 필수입니다.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+    })
 	public List<ChatMessageListResponseDto> findChatMessages(Authentication authentication, @Valid @RequestBody ChatMessageListRequestDto chatMessageListRequestDto) {
 		// Authentication 에서 로그인한 유저의 userId 추출
         Claims claims = (Claims)authentication.getPrincipal();
