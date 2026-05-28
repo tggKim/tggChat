5186c3a8be6fc890e8eb15ec7b52888e2ff7aecf
tgg <qazplmtfgcv123@gmail.com>
2026-05-28 17:22:57 +0900

    test: AccessTokenAuthenticator 토큰 검증 성공 테스트 추가

---
+++ src/test/java/com/tgg/chat/common/security/jwt/AccessTokenAuthenticatorTest.java
@@ -0,0 +1,59 @@
+package com.tgg.chat.common.security.jwt;
+
+import com.tgg.chat.common.security.principal.AuthenticatedUser;
+import com.tgg.chat.common.security.token.RedisTokenStore;
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccessTokenAuthenticatorTest {
+    @Mock
+    JwtUtils jwtUtils;
+
+    @Mock
+    RedisTokenStore redisTokenStore;
+
+    @InjectMocks
+    AccessTokenAuthenticator accessTokenAuthenticator;
+
+    @Test
+    @DisplayName("토큰 검증 성공")
+    void authenticate_token_success() {
+        // given
+        String bearerString = "Bearer token";
+
+        Claims claims = mock(Claims.class);
+
+        when(jwtUtils.parseClaims("token")).thenReturn(claims);
+
+        when(claims.getSubject()).thenReturn("1");
+
+        when(redisTokenStore.matchesAccessToken(1L, "token")).thenReturn(true);
+
+        when(claims.get("email", String.class)).thenReturn("test@test.com");
+        when(claims.get("username", String.class)).thenReturn("testUsername");
+
+        // when
+        AuthenticatedUser authenticatedUser = accessTokenAuthenticator.authenticateBearerToken(bearerString);
+
+        // then
+        assertThat(authenticatedUser.getUserId()).isEqualTo(1L);
+        assertThat(authenticatedUser.getEmail()).isEqualTo("test@test.com");
+        assertThat(authenticatedUser.getUsername()).isEqualTo("testUsername");
+
+        verify(jwtUtils, times(1)).parseClaims("token");
+        verify(claims, times(1)).getSubject();
+        verify(redisTokenStore, times(1)).matchesAccessToken(1L, "token");
+        verify(claims, times(1)).get("email", String.class);
+        verify(claims, times(1)).get("username", String.class);
+    }
+}
