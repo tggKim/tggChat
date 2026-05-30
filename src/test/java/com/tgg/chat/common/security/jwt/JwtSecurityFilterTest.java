c9c9c898e3aa40b03dc70d054f967bbe877a35f5
tgg <qazplmtfgcv123@gmail.com>
2026-05-30 18:50:47 +0900

    test: JWT 필터 인증 예외 응답 검증 추가

--- src/test/java/com/tgg/chat/common/security/jwt/JwtSecurityFilterTest.java
+++ src/test/java/com/tgg/chat/common/security/jwt/JwtSecurityFilterTest.java
@@ -1,7 +1,10 @@
 package com.tgg.chat.common.security.jwt;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tgg.chat.common.security.principal.AuthenticatedUser;
+import com.tgg.chat.exception.ErrorCode;
+import com.tgg.chat.exception.ErrorException;
 import jakarta.servlet.FilterChain;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +17,8 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -70,5 +75,35 @@ class JwtSecurityFilterTest {
 
         verify(accessTokenAuthenticator, times(1)).authenticateBearerToken("Bearer AccessToken");
         verify(filterChain, times(1)).doFilter(request, response);
+    }
+
+    @Test
+    @DisplayName("JWT 필터 실패 - 인증 예외 발생")
+    void jwtSecurityFilter_fail_authentication_exception() throws Exception {
+        // given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", "invalidToken");
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(accessTokenAuthenticator.authenticateBearerToken("invalidToken")).thenThrow(new ErrorException(ErrorCode.JWT_INVALID_TOKEN));
+
+        // when
+        jwtSecurityFilter.doFilter(request, response, filterChain);
+
+        // then
+        assertThat(SecurityContextHolder.getContext().getAuthentication()).isNull();
+
+        assertThat(response.getStatus()).isEqualTo(ErrorCode.JWT_INVALID_TOKEN.getStatus().value());
+        assertThat(response.getContentType()).isEqualTo("application/json; charset=UTF-8");
+
+        JsonNode jsonNode = objectMapper.readTree(response.getContentAsString(StandardCharsets.UTF_8));
+        assertThat(jsonNode.get("code").asText()).isEqualTo(ErrorCode.JWT_INVALID_TOKEN.getCode());
+        assertThat(jsonNode.get("status").asInt()).isEqualTo(ErrorCode.JWT_INVALID_TOKEN.getStatus().value());
+        assertThat(jsonNode.get("message").asText()).isEqualTo(ErrorCode.JWT_INVALID_TOKEN.getMessage());
+        assertThat(jsonNode.get("timestamp").asText()).isNotBlank();
+
+        verify(accessTokenAuthenticator, times(1)).authenticateBearerToken("invalidToken");
+        verify(filterChain, never()).doFilter(request, response);
     }
 }
