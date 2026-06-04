e194b2fa1817dedb9800d959e317f99e86990bfa
tgg <qazplmtfgcv123@gmail.com>
2026-06-04 20:26:23 +0900

    refactor: 미사용 UserMapper 조회 메서드 제거

--- src/main/java/com/tgg/chat/domain/user/repository/UserMapper.java
+++ src/main/java/com/tgg/chat/domain/user/repository/UserMapper.java
@@ -5,11 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.tgg.chat.domain.chat.dto.query.UserIdUsernameQueryDto;
-import com.tgg.chat.domain.user.entity.User;
 
 @Mapper
 public interface UserMapper {
-	User findById(Long userId);
-	
 	List<UserIdUsernameQueryDto> getUserNames(List<Long> userIds);
 }
