49fd82cd0d4efecb700eb8ecdc5980f257d372f3
tgg <qazplmtfgcv123@gmail.com>
2026-06-09 17:02:37 +0900

    refactor: RedisTokenStore 원래 상태로 복구

--- src/main/java/com/tgg/chat/common/security/token/RedisTokenStore.java
+++ src/main/java/com/tgg/chat/common/security/token/RedisTokenStore.java
@@ -11,32 +11,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RedisTokenStore {
 
-	private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
-	private static final String RT_PREFIX = "RT:";
-	
-	public void saveRefreshToken(String sid, String refreshToken, long ttlMilliseconds) {
-		String key = createRefreshTokenKey(sid);
-		redisTemplate.opsForValue().set(key, refreshToken, ttlMilliseconds, TimeUnit.MILLISECONDS);
-	}
-	
-	public boolean matchesRefreshToken(String sid, String refreshToken) {
-		String key = createRefreshTokenKey(sid);
-		String redisRefreshToken = redisTemplate.opsForValue().get(key);
-		if(redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
-			return false;
-		}
-		
-		return true;
-	}
-	
-	public void deleteRefreshToken(String sid) {
-		String refreshTokenKey = createRefreshTokenKey(sid);
-		redisTemplate.delete(refreshTokenKey);
-	}
-	
-	private String createRefreshTokenKey(String sid) {
-		return RT_PREFIX + sid;
-	}
-	
+    private static final String RT_PREFIX = "RT:";
+
+    public void saveRefreshToken(String sid, String refreshToken, long ttlMilliseconds) {
+        String key = createRefreshTokenKey(sid);
+        redisTemplate.opsForValue().set(key, refreshToken, ttlMilliseconds, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean matchesRefreshToken(String sid, String refreshToken) {
+        String key = createRefreshTokenKey(sid);
+        String redisRefreshToken = redisTemplate.opsForValue().get(key);
+        if(redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void deleteRefreshToken(String sid) {
+        String refreshTokenKey = createRefreshTokenKey(sid);
+        redisTemplate.delete(refreshTokenKey);
+    }
+
+    private String createRefreshTokenKey(String sid) {
+        return RT_PREFIX + sid;
+    }
+
 }
