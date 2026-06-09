1661ce70f4f4849664ab673f0cc5b17e5185abc8
tgg <qazplmtfgcv123@gmail.com>
2026-06-09 13:08:04 +0900

    refactor: Redis 토큰 저장소를 세션 ID 기반 리프레시 토큰 관리로 변경

--- src/main/java/com/tgg/chat/common/security/token/RedisTokenStore.java
+++ src/main/java/com/tgg/chat/common/security/token/RedisTokenStore.java
@@ -12,39 +12,22 @@ import lombok.RequiredArgsConstructor;
 public class RedisTokenStore {
 
 	private final RedisTemplate<String, String> redisTemplate;
-	
-	private static final String AT_PREFIX = "AT:";
+
 	private static final String RT_PREFIX = "RT:";
 	
-	public void saveAccessToken(Long userId, String accessToken, long ttlMilliseconds) {
-		String key = createAccessTokenKey(userId);
-		redisTemplate.opsForValue().set(key, accessToken, ttlMilliseconds, TimeUnit.MILLISECONDS);
-	}
-	
-	public void saveRefreshToken(Long userId, String refreshToken, long ttlMilliseconds) {	
-		String key = createRefreshTokenKey(userId);	
+	public void saveRefreshToken(String sid, String refreshToken, long ttlMilliseconds) {
+		String key = createRefreshTokenKey(sid);
 		redisTemplate.opsForValue().set(key, refreshToken, ttlMilliseconds, TimeUnit.MILLISECONDS);
 	}
-
-	public boolean matchesAccessToken(Long userId, String accessToken) {
-		String key = createAccessTokenKey(userId);
-		String redisAccessToken = redisTemplate.opsForValue().get(key);
-		
-		if(redisAccessToken == null || !redisAccessToken.equals(accessToken)) {
-			return false;
-		}
-		
-		return true;
-	}
 	
-	public boolean hasRefreshToken(Long userId) {
-		String key = createRefreshTokenKey(userId);	
+	public boolean hasRefreshToken(String sid) {
+		String key = createRefreshTokenKey(sid);
 		String redisRefreshToken = redisTemplate.opsForValue().get(key);
 		return redisRefreshToken != null ? true : false;
 	}
 	
-	public boolean matchesRefreshToken(Long userId, String refreshToken) {
-		String key = createRefreshTokenKey(userId);	
+	public boolean matchesRefreshToken(String sid, String refreshToken) {
+		String key = createRefreshTokenKey(sid);
 		String redisRefreshToken = redisTemplate.opsForValue().get(key);
 		if(redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
 			return false;
@@ -53,21 +36,13 @@ public class RedisTokenStore {
 		return true;
 	}
 	
-	public void deleteUserTokenSets(Long userId) {
-		String accessTokenKey = createAccessTokenKey(userId);
-		String refreshTokenKey = createRefreshTokenKey(userId);
-		redisTemplate.delete(accessTokenKey);
+	public void deleteRefreshToken(String sid) {
+		String refreshTokenKey = createRefreshTokenKey(sid);
 		redisTemplate.delete(refreshTokenKey);
 	}
 	
-	private String createAccessTokenKey(Long userId) {
-		String key = AT_PREFIX + userId;	
-		return key;
-	}
-	
-	private String createRefreshTokenKey(Long userId) {
-		String key = RT_PREFIX + userId;	
-		return key;
+	private String createRefreshTokenKey(String sid) {
+		return RT_PREFIX + sid;
 	}
 	
 }
