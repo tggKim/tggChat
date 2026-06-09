41e4e86aebe851a97e3a79219667eb546a1dc4e1
tgg <qazplmtfgcv123@gmail.com>
2026-06-09 12:53:33 +0900

    refactor: 인증 주체를 유저 ID와 세션 ID 중심으로 변경

--- src/main/java/com/tgg/chat/common/security/principal/AuthenticatedUser.java
+++ src/main/java/com/tgg/chat/common/security/principal/AuthenticatedUser.java
@@ -5,11 +5,10 @@ import lombok.Getter;
 @Getter
 public class AuthenticatedUser {
     private final Long userId;
-    private final String email;
-    private final String username;
-    public AuthenticatedUser(Long userId, String email, String username) {
+    private final String sid;
+
+    public AuthenticatedUser(Long userId, String sid) {
         this.userId = userId;
-        this.email = email;
-        this.username = username;
+        this.sid = sid;
     }
 }
