9f96ff3109994f883fbfbd0dfdb945657e82926f
tgg <qazplmtfgcv123@gmail.com>
2026-06-23 15:02:55 +0900

    refactor: ErrorException 원인 예외 보존 생성자 추가

--- src/main/java/com/tgg/chat/exception/ErrorException.java
+++ src/main/java/com/tgg/chat/exception/ErrorException.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 
 @Getter
 public class ErrorException extends RuntimeException {
-	
 	private final ErrorCode errorCode;
 	
 	public ErrorException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
 	}
-	
+
+	public ErrorException(ErrorCode errorCode, Throwable throwable) {
+        super(errorCode.getMessage(), throwable);
+        this.errorCode = errorCode;
+    }
 }
