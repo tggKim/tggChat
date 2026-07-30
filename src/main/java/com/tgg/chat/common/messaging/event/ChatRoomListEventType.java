b51d70478d6ca3e66ce409c8ed94ce66a89436a2
tgg <qazplmtfgcv123@gmail.com>
2026-07-30 14:24:52 +0900

    feat: 채팅방 목록 읽음 상태 갱신 이벤트 타입 추가
    - ChatRoomListEventType에 MESSAGE_READ 추가
    - 채팅방 목록 이벤트 타입 순서 정리

--- src/main/java/com/tgg/chat/common/messaging/event/ChatRoomListEventType.java
+++ src/main/java/com/tgg/chat/common/messaging/event/ChatRoomListEventType.java
@@ -2,7 +2,8 @@ package com.tgg.chat.common.messaging.event;
 
 public enum ChatRoomListEventType {
     ROOM_ADDED,
-    ROOM_REMOVED,
     ROOM_CHANGED,
-    MESSAGE_SENT
+    ROOM_REMOVED,
+    MESSAGE_SENT,
+    MESSAGE_READ
 }
