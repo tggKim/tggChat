e84d45a1326a28c3e9b4f9190d12df368cb1c2d6
tgg <qazplmtfgcv123@gmail.com>
2026-07-31 22:15:17 +0900

    style: CreateDirectChatRoomRequestDto 불필요한 공백 제거

--- src/main/java/com/tgg/chat/domain/chat/dto/request/CreateDirectChatRoomRequestDto.java
+++ src/main/java/com/tgg/chat/domain/chat/dto/request/CreateDirectChatRoomRequestDto.java
@@ -9,9 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(description = "1대1 채팅방 생성 요청 DTO")
 public class CreateDirectChatRoomRequestDto {
-
     @Schema(description = "채팅방을 만들 상대 userId", example = "1")
     @NotNull(message = "friendId 는 필수입니다.")
     private Long friendId;
-
 }
