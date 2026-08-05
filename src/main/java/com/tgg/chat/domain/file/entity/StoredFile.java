82ec9985ea98672c1315099cb826bfd1b764a290
tgg <qazplmtfgcv123@gmail.com>
2026-08-05 19:02:57 +0900

    feat: 파일 메타데이터와 표시 순서를 관리하는 StoredFile 엔티티 추가

---
+++ src/main/java/com/tgg/chat/domain/file/entity/StoredFile.java
@@ -0,0 +1,84 @@
+package com.tgg.chat.domain.file.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        indexes = {
+                @Index(
+                        name = "idx_file_key_file_order",
+                        columnList = "file_key,file_order"
+                )
+        }
+)
+public class StoredFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storedFileId;
+
+    @Column(nullable = false, updatable = false)
+    private String fileKey;
+
+    @Column(nullable = false, updatable = false)
+    private String storedFileName;
+
+    @Column(nullable = false, updatable = false)
+    private String originalFileName;
+
+    @Column(nullable = false, updatable = false)
+    private String contentType;
+
+    @Column(nullable = false, updatable = false)
+    private Long fileSize;
+
+    @Column(nullable = false, updatable = false)
+    private Integer fileOrder;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private StoredFile(
+            String fileKey,
+            String storedFileName,
+            String originalFileName,
+            String contentType,
+            Long fileSize,
+            Integer fileOrder
+    ) {
+        this.fileKey = fileKey;
+        this.storedFileName = storedFileName;
+        this.originalFileName = originalFileName;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.fileOrder = fileOrder;
+    }
+
+    public static StoredFile of(
+            String fileKey,
+            String storedFileName,
+            String originalFileName,
+            String contentType,
+            Long fileSize,
+            Integer fileOrder
+    ) {
+        return new StoredFile(
+                fileKey,
+                storedFileName,
+                originalFileName,
+                contentType,
+                fileSize,
+                fileOrder
+        );
+    }
+}
