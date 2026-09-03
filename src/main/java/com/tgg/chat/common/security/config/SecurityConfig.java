6819a141e32805afbf2ee059fc61c1407b644141
tgg <qazplmtfgcv123@gmail.com>
2026-09-03 23:47:56 +0900

    refactor: cors 설정 제거

--- src/main/java/com/tgg/chat/common/security/config/SecurityConfig.java
+++ src/main/java/com/tgg/chat/common/security/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://tggchat.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
